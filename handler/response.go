581307c82e1fadea47c7dc243ed5e3b39d38cdcb
fchieff <lolfbl3@gmail.com>
2025-02-04 22:18:29 -0300

    feat: add req and res helpers

---
+++ handler/response.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, code int, message string) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(code, gin.H{
+		"error":   message,
+		"errCode": code,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, operation string, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"data":    data,
+		"message": fmt.Sprintf("operation %s success", operation),
+	})
+}
